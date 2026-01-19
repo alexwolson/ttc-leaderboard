1d05148279db44691bebe8cba8495ee53432448c
nelimalu <lukaj2501@gmail.com>
2026-01-19 13:20:20 -0500

    working version v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import LeaderboardPosition from './components/LeaderboardPosition'
+
+interface LeaderboardData {
+  routeNumber: string;
+  speed: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [leaderboardData, setLeaderboardData] = useState<LeaderboardData[]>([]);
+  const route_map: { [key: string]: string } = {
+    "501": "Queen",
+    "503": "Kingston",
+    "504": "King",
+    "505": "Dundas",
+    "506": "Carlton",
+    "507": "Long Branch",
+    "508": "Lake Shore",
+    "509": "Harbourfront",
+    "510": "Spadina",
+    "511": "Bathurst",
+    "512": "St. Clair"
+  };
+
+  const fetchLeaderboard = async () => {
+    try {
+      const response = await fetch('/api/ttc');
+      const data = await response.json();
+
+      if (response.status !== 200)
+        throw new Error(`Failed to fetch: ${response.status}`);
+
+      setLeaderboardData(data.map((route: [string, number]) => ({
+        routeNumber: route[0],
+        speed: route[1]
+      })));
+
+    } catch (error) {
+      console.error('Error fetching leaderboard:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLeaderboard();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="wrapper">
+        <div className="title">
+          TTC STREETCARS LIVE LEADERBOARD
+        </div>
+        <div className="image-container">
+          <img id="streetcar-image" src="https://farm7.static.flickr.com/6184/6125705401_815fe729e4_o.jpg" alt="streetcar pulled by horse" />
+        </div>
+        <div className="information">
+          Recently the TTC has been under a lot of criticism for <a href="https://www.blogto.com/city/2024/08/toronto-ttc-streetcars-slowest-world/" target="_blank">slow service</a>.
+          <br></br>
+          This site intends to show how slow it really is.
+        </div>
+        <div className="leaderboard">
+          {leaderboardData.map((position, index) => (
+            <LeaderboardPosition key={index} routeNumber={position.routeNumber} routeName={route_map[position.routeNumber]} speed={position.speed} />
+          ))}
+        </div>
+        <div className="footer">
+          <br></br>
+          <i>By <a href="https://lukajvnic.com" target="_blank">Luka Jovanovic</a></i>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
---
+++ src/components/LeaderboardPosition.tsx
@@ -0,0 +1,35 @@
+import './LeaderboardPosition.css'
+
+interface LeaderboardPosition {
+    routeNumber: string;
+    routeName: string;
+    speed: number;
+}
+
+
+function LeaderboardPosition({ routeNumber, routeName, speed }: LeaderboardPosition) {
+    return (
+        <div className="leaderboard-position">
+            <div className="border">
+                +--------------------------------------------------+
+            </div>
+            <div className="content">
+                <div className="left-side">
+                    |&nbsp;
+                    <div className="position-route-number">{routeNumber}</div>
+                    &nbsp;-&nbsp;
+                    <div className="position-route-name">{routeName}</div>
+                </div>
+                <div className="right-side">
+                    <div className="position-speed">{speed.toFixed(1)} km/h</div>
+                    &nbsp;|
+                </div>
+            </div>
+            <div className="border">
+                +--------------------------------------------------+
+            </div>
+        </div>
+    );
+}
+
+export default LeaderboardPosition;
